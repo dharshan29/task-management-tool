f9cbfcdbe28697130b52a8f092dc4e7c6cf67acd
dharshan29 <dharshangowda2000@gmail.com>
2025-02-16 00:21:13 +0530

    bug

--- src/app/providers-wrapper.tsx
+++ src/app/providers-wrapper.tsx
@@ -1,8 +1,14 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import Providers from '@/lib/providers';
 
 export default function ProvidersWrapper({ children }: { children: React.ReactNode }) {
-  const hasToken = localStorage.getItem('token') !== null;
-  return  <Providers isAuth={hasToken}>{children}</Providers>
+  const [hasToken, setHasToken] = useState(false);
+
+  useEffect(() => {
+    setHasToken(localStorage.getItem('token') !== null);
+  }, []);
+
+  return <Providers isAuth={hasToken}>{children}</Providers>;
 }
