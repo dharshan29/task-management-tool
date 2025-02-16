7882959981efc4cf2d13502d38c8930a125add3a
dharshan29 <dharshangowda2000@gmail.com>
2025-02-16 11:30:05 +0530

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './'),
+    };
+
+    return config;
+  },
   images: {
     domains: ["lh3.googleusercontent.com"], // ✅ Allow Google-hosted images
   },
